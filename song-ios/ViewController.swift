d6417fffb75eb5c2f204e36d7f7585a755d3da58
booy <xxhushaomin@gmail.com>
2015-08-19 15:56:57 +0800

    Created

---
+++ song-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  song-ios
+//
+//  Created by booy on 15/8/19.
+//  Copyright (c) 2015年 Aspiring Citizens Cleantech Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
